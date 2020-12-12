0bd8061a44e9b100519123efcbd32030526c33a1
Daniel Mendler <mail@daniel-mendler.de>
2020-12-12 08:09:10 +0100

    marginalia--fields: allow alignment to the right by using a negative width

--- marginalia.el
+++ marginalia.el
@@ -253,7 +253,7 @@ FORMAT is a format string. This must be used if the field value is not a string.
 FACE is the name of the face, with which the field should be propertized.
 WIDTH is the format width. This can be specified as alternative to FORMAT."
   (cl-assert (not (and width format)))
-  (when width (setq format (format "%%-%ds" width)))
+  (when width (setq format (format "%%%ds" (- width))))
   (when format (setq field `(format ,format ,field)))
   (when truncate (setq field `(marginalia--truncate ,field ,truncate)))
   (when face (setq field `(propertize ,field 'face ,face)))
