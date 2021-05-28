e97979a24969fed3c04241ef442fb5ba6a5fc401
Daniel Mendler <mail@daniel-mendler.de>
2021-05-28 08:49:17 +0200

    Remove License header

--- marginalia.el
+++ marginalia.el
@@ -5,7 +5,6 @@
 ;; Author: Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2020
-;; License: GPL-3.0-or-later
 ;; Version: 0.5
 ;; Package-Requires: ((emacs "26.1"))
 ;; Homepage: https://github.com/minad/marginalia
