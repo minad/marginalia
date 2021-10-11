37e24b798afca98da0d0364dde3fa63a42c5853e
Daniel Mendler <mail@daniel-mendler.de>
2021-10-11 09:19:52 +0200

    Version 0.9

--- marginalia.el
+++ marginalia.el
@@ -5,7 +5,7 @@
 ;; Author: Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2020
-;; Version: 0.8
+;; Version: 0.9
 ;; Package-Requires: ((emacs "26.1"))
 ;; Homepage: https://github.com/minad/marginalia
 
