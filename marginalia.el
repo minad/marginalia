796acf6329174909ee7e3f03fe4ed12e058a3f81
Daniel Mendler <mail@daniel-mendler.de>
2025-12-23 16:07:47 +0100

    Extend commentary

--- marginalia.el
+++ marginalia.el
@@ -27,7 +27,11 @@
 
 ;;; Commentary:
 
-;; Enrich existing commands with completion annotations
+;; Enrich existing commands with completion annotations.  The information
+;; associated with the completion candidates is shown in the minibuffer or the
+;; *Completions* buffer.  For files the owner and permissions are shown, for
+;; buffers the major mode and modification status and for functions or commands
+;; the docstring is shown.
 
 ;;; Code:
 
